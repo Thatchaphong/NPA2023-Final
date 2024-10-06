c784def5ae963272fff2ae52bc3dd34fc6aaa0bd
Thatchaphong <65070104@kmitl.ac.th>
2024-10-06 17:50:11 +0000

    Finish Final Exam Practice

--- npa2023_final.py
+++ npa2023_final.py
@@ -1,24 +1,25 @@
 #######################################################################################
-# Yourname:
-# Your student ID:
-# Your GitHub Repo: 
+# Yourname: Thatchaphong Phaisanthanaphon
+# Your student ID: 65070104
+# Your GitHub Repo: https://github.com/Thatchaphong/NPA2023-Final.git
 
 #######################################################################################
 # 1. Import libraries for API requests, JSON formatting, time, and (restconf_final or netconf_final).
-
-<!!!REPLACEME with code for libraries>
-
+import requests
+import json
+import time
+from restconf_final import create, delete, enable, disable, status
 #######################################################################################
 # 2. Assign the Webex hard-coded access token to the variable accessToken.
 
-accessToken = "Bearer <!!!REPLACEME with hard-coded token!!!>"
+accessToken = "Bearer <accessToken>"
 
 #######################################################################################
 # 3. Prepare parameters get the latest message for messages API.
 
 # Defines a variable that will hold the roomId
 roomIdToGetMessages = (
-    "<!!!REPLACEME with roomID of the NPA2023 Webex Teams room!!!>"
+    "<roomid>"
 )
 
 while True:
@@ -31,17 +32,17 @@ while True:
     getParameters = {"roomId": roomIdToGetMessages, "max": 1}
 
     # the Webex Teams HTTP header, including the Authoriztion
-    getHTTPHeader = {"Authorization": <!!!REPLACEME!!!>}
+    getHTTPHeader = {"Authorization": accessToken}
 
 # 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
-    
+
     # Send a GET request to the Webex Teams messages API.
     # - Use the GetParameters to get only the latest message.
     # - Store the message in the "r" variable.
     r = requests.get(
-        "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-        params=<!!!REPLACEME with HTTP parameters!!!>,
-        headers=<!!!REPLACEME with HTTP headers!!!>,
+        "https://webexapis.com/v1/messages",
+        params=getParameters,
+        headers=getHTTPHeader,
     )
     # verify if the retuned HTTP status code is 200/OK
     if not r.status_code == 200:
@@ -65,44 +66,46 @@ while True:
 
     # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
     #  e.g.  "/66070123 create"
-    if message.find("<!!!REPLACEME!!!>") == 0:
+    if message.find("/65070104") == 0:
 
         # extract the command
-        command = <!!!REPLACEME!!!>
+        command = message.split(" ", 1)
+        command = command[1]
         print(command)
 
 # 5. Complete the logic for each command
 
         if command == "create":
-            <!!!REPLACEME with code for create command!!!>     
+            responseMessage = create()
         elif command == "delete":
-            <!!!REPLACEME with code for delete command!!!>
+            responseMessage =  delete()
         elif command == "enable":
-            <!!!REPLACEME with code for enable command!!!>
+            responseMessage = enable()
         elif command == "disable":
-            <!!!REPLACEME with code for disable command!!!>
+            responseMessage = disable()
         elif command == "status":
-            <!!!REPLACEME with code for status command!!!>
+            responseMessage = status()
         else:
             responseMessage = "Error: No command or unknown command"
-        
+
 # 6. Complete the code to post the message to the Webex Teams room.
-        
+
         # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-        postHTTPHeaders = HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}
+        postHTTPHeaders = HTTPHeaders = {"Authorization": accessToken, "Content-Type": "application/json"}
 
         # The Webex Teams POST JSON data
         # - "roomId" is is ID of the selected room
         # - "text": is the responseMessage assembled above
-        postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
+        postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
 
         # Post the call to the Webex Teams message API.
         r = requests.post(
-            "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-            data=json.dumps(<!!!REPLACEME!!!>a),
-            headers=<!!!REPLACEME!!!>,
+            "https://webexapis.com/v1/messages",
+            data=json.dumps(postData),
+            headers=postHTTPHeaders,
         )
         if not r.status_code == 200:
             raise Exception(
                 "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
             )
+
--- restconf_final.py
+++ restconf_final.py
@@ -2,105 +2,139 @@ import json
 import requests
 requests.packages.urllib3.disable_warnings()
 
-# Router IP Address is 10.0.15.189
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+# Router IP Address is 
+api_url = "https://<ip address>/restconf"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
+headers = { "Accept": "application/yang-data+json",
+            "Content-type": "application/yang-data+json"
+           }
 basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback65070104",
+            "description": "create loopback",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "172.30.104.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            },
+            "ietf-ip:ipv6": {}
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        api_url+"/data/ietf-interfaces:interfaces/interface=Loopback65070104", 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
-
-    if(resp.status_code >= 200 and resp.status_code <= 299):
+    if(resp.status_code == 204):
+        return "Cannot create: Interface loopback 65070104"
+    elif(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface loopback 65070104 is created successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
 
 
 def delete():
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
+    resp = requests.delete(
+        api_url+"/data/ietf-interfaces:interfaces/interface=Loopback65070104", 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "loopback 65070104 is deleted successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return "Cannot delete: Interface loopback 65070104"
 
 
 def enable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback65070104",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+
+    resp = requests.patch(
+        api_url+"/data/ietf-interfaces:interfaces/interface=Loopback65070104",
+        data=json.dumps(yangConfig),
+        auth=basicauth,
+        headers=headers,
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface loopback 65070104 is enabled successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
-
+        return "Cannot enable: Interface loopback 65070104"
 
 def disable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback65070104",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": False,
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+    resp = requests.patch(
+        api_url+"/data/ietf-interfaces:interfaces/interface=Loopback65070104",
+        data=json.dumps(yangConfig),
+        auth=basicauth,
+        headers=headers,
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface loopback 65070104 is disabled"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
-
+        return "Cannot shutdown: Interface loopback 65070104"
 
 def status():
-    api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
+    api_url_status = "https://10.0.15.111/restconf/data/ietf-interfaces:interfaces-state/interface=Loopback65070104"
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+    resp = requests.get(
+        api_url_status,
+        auth=basicauth,
+        headers=headers,
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
         response_json = resp.json()
-        admin_status = <!!!REPLACEME!!!>
-        oper_status = <!!!REPLACEME!!!>
+        #print(json.dumps(response_json, indent=4))
+        admin_status = response_json["ietf-interfaces:interfaces-state"]["interface"][0]["admin-status"]
+        oper_status = response_json["ietf-interfaces:interfaces-state"]["interface"][0]["oper-status"]
         if admin_status == 'up' and oper_status == 'up':
-            return "<!!!REPLACEME with proper message!!!>"
+            return "Interface loopback65070104 is enabled"
         elif admin_status == 'down' and oper_status == 'down':
-            return "<!!!REPLACEME with proper message!!!>"
+            return "Interface loopback65070104 is disabled"
     elif(resp.status_code == 404):
         print("STATUS NOT FOUND: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "No Interface loopback 65070104"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
